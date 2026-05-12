048a60c46083407ea2d96dd7ca38b2ac64c0eeb4
l3gium <miguel.luiztto@gmail.com>
2026-05-12 00:45:34 -0300

    feat: IUserRepository

---
+++ Finantial_Manager_API/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Finantial_Manager_API.Models;
+
+namespace Finantial_Manager_API.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByEmailAsync(string email);
+        Task<User?> GetByIdAsync(Guid id);
+        Task<bool> EmailExistsAsync(string email);
+        Task<User> CreateAsync(User user);
+    }
+}
