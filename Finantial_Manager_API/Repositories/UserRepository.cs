6fbd18ad47dc0fc507da7b9d39a17b6d22423994
l3gium <miguel.luiztto@gmail.com>
2026-05-12 00:53:49 -0300

    feat: user repository

---
+++ Finantial_Manager_API/Repositories/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using Finantial_Manager_API.Data;
+using Finantial_Manager_API.Models;
+using Finantial_Manager_API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finantial_Manager_API.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context) => _context = context;
+
+        public async Task<User> CreateAsync(User user)
+        {
+            user.Email = user.Email.ToLower();
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<bool> EmailExistsAsync(string email)
+        {
+            return await _context.Users
+                .AnyAsync(u => u.Email == email.ToLower());
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == email.ToLower() && u.IsActive);
+        }
+
+        public async Task<User?> GetByIdAsync(Guid id)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(u => u.Id == id && u.IsActive);
+        }
+    }
+}
